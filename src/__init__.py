653be31b2ab01ff4b05c062bb9e4322020675d38
Carson Poole <carson@poole.ai>
2025-12-20 15:56:42 +0000

    integrate fp4 linear into `quantize.py`

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .world_engine import WorldEngine, CtrlInput
-from .quantize import QUANTS
-from .fp4_linear import FP4Linear, HAS_FP4, patch_linear_to_fp4
+from .quantize import QUANTS, FP4Linear, HAS_FP4, patch_linear_to_fp4
 
 __all__ = [
     "WorldEngine",
